706b69f9557e6f3e379de602c4fbdc222e5f1a9f
Nehemiah Mekonnen <binyamm83@gmail.com>
2025-01-03 16:01:56 +0300

    instructor crud implemented

--- core/models.py
+++ core/models.py
@@ -70,7 +70,6 @@ class Course(models.Model):
     description = models.TextField(blank=True, null=True)
     department = models.ForeignKey(Department, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
-    is_active = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
